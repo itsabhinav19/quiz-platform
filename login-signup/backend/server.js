bb6be57e98a264037d8e11d9f561f3ea704b6ddc
itsabhinav19 <abhinavkashyap0522@gmail.com>
2025-12-30 15:59:16 +0530

    Fix rate-limit trust proxy issue for Render

--- login-signup/backend/server.js
+++ login-signup/backend/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const rateLimit = require("express-rate-limit");
 const authRoutes = require("./routes/auth");
 const adminRoutes = require("./routes/admin");
+const PORT = process.env.PORT || 5000;
 require("dotenv").config();
 
 const app = express();
 
+app.set("trust proxy", 1);
+const rateLimit = require("express-rate-limit");
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100,                // 100 requests per IP
@@ -32,6 +34,9 @@ app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+// app.listen(5000, () => {
+//   console.log("Server running on http://localhost:5000");
+// });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
